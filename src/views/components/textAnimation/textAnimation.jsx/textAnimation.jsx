f7d0d9ded475097c104860bc6ec35dd271c3ead5
SheshantManure <sheshantmanure@ve.co>
2024-05-23 13:10:18 +0530

    Waveform smooth transition

--- src/views/components/textAnimation/textAnimation.jsx/textAnimation.jsx
+++ src/views/components/textAnimation/textAnimation.jsx/textAnimation.jsx
@@ -5,7 +5,7 @@ const TextAnimation = () => {
     const [phase, setPhase] = useState(0);
     const textRef = useRef(null);
 
-    const textContent = "Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE";
+    const textContent = "Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE Photo by - VE Arjun & Aanya Wedding Collections Represented by VE All images copyright 2024 Photo by - VE Arjun & Aanya Wedding Collections Represented by VE";
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -26,12 +26,12 @@ const TextAnimation = () => {
         const baseHeight = 50;
 
         const waveParams = [
-            { amplitude: 5, frequency: 0.01 },
-            { amplitude: 7, frequency: 0.02 },
+            { amplitude: 5, frequency: 0.001 },
+            { amplitude: 7, frequency: 0.002 },
             { amplitude: 5,  frequency: 0.02 },
         ];
 
-        for (let x = 0; x <= 1000; x += 10) {
+        for (let x = 0; x <= 3000; x += 10) {
             let y = baseHeight;
             waveParams.forEach((wave, index) => {
                 y += wave.amplitude * Math.sin(wave.frequency * x + currentPhase + index * Math.PI / 4);
@@ -45,15 +45,20 @@ const TextAnimation = () => {
 
     return (
         <div className={style.container}>
-            <svg xmlns="http://www.w3.org/2000/svg">
-                <path id="wavePath" fill="transparent" stroke="transparent"
-                      d={generateWavePath(phase)} />
-                <text ref={textRef} className={style.text} fontSize="18" fill="black">
-                    <textPath href="#wavePath" startOffset="0">
-                        {textContent}
-                    </textPath>
-                </text>
-            </svg>
+            <div className={style.main}>
+                <svg xmlns="http://www.w3.org/2000/svg">
+                    <path id="wavePath" fill="transparent" stroke="transparent"
+                        d={generateWavePath(phase)} />
+                    <text ref={textRef} className={style.text} fontSize="18" fill="black">
+                        <textPath href="#wavePath" startOffset="0">
+                            {textContent}
+                        </textPath>
+                        <textPath href="#wavePath" startOffset="100%">
+                            {textContent}
+                        </textPath>
+                    </text>
+                </svg>
+            </div>
         </div>
     );
 };
